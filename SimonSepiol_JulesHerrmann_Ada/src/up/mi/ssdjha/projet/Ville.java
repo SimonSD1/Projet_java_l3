a97259b665f02f285311f8bcf6d729f710f9bed7
Ada-jm <148342280+Ada-jm@users.noreply.github.com>
2023-10-31 14:12:29 +0100

    Update Ville.java
    Pour l'instant, j'ai fais ça, mais si vous voulez ajouter des modifications, allez-y.

--- SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/Ville.java
+++ SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/Ville.java
@@ -2,4 +2,40 @@ package up.mi.ssdjha.projet;
 
 public class Ville {
 
+  public int n; // Nombre de villes (26 max)
+	public String nom_villes;
+	//public String villes[];
+	
+	//J'ai mis les attributs en public au cas où on pour ajouter les routes
+	//si on doit faire un héritage
+	
+	
+	public Ville(int n, String nom_villes) {
+		this.n = n;
+		this.nom_villes = nom_villes;
+	}
+	
+	public int nombreVille(int e) {
+		System.out.printf("Entrez le nombre de ville(s) que vous voulez");
+		ajoutVille(e);
+		return e;
+	}
+	
+	public boolean ajoutVille(int m) {
+		
+		if(m>0 && m<26) {
+			System.out.printf("Vous pouvez ajouter cette ville");
+			return true;
+		}
+		
+		else {
+			do {
+				System.out.printf("Entrer le nombre de ville(s) que vous voulez créer (max 26)");
+			}while(m<26);
+			
+			return false;
+			
+		}		
+		
+
 }
