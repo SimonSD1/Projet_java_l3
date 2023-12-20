29ce64bd5d0d31f2098bc0779626645d674a4f0a
Ada-jm <148342280+Ada-jm@users.noreply.github.com>
2023-12-20 13:17:03 +0100

    Update UnorderedPair.java
    Commentaires ajoutés

--- SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/UnorderedPair.java
+++ SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/UnorderedPair.java
@@ -19,19 +19,39 @@ public class UnorderedPair<T> {
 		second = b;
 	}
 
+	/** Méthode qui permet de retourner avec
+	 * @return la 1ère paire.**/
+	
 	public T getFirst(){
 		return first;
 	}
+
+	/** Méthode qui permet de retourner avec
+	 * @return la 2ème paire.**/
+	
 	public T getSecond(){
 		return second;
 	}
 
+	/**Méthode qui
+	 * @return true si la pair passer en paramètre est égale à la paire
+	 * qu'on essaye de créer.**/
+	
 	public boolean equals(Object o) {
 		if (getClass() != o.getClass()) return false;
 		UnorderedPair b = (UnorderedPair) o;
 		return (this.first.equals(b.getFirst()) && this.second.equals(b.getSecond())) || (this.second.equals(b.getFirst()) && this.first.equals(b.getSecond()));
 	}
 
+	
+	/**Méthode qui permet de faire comprendre à la machine que, par exemple
+	 *  "UnorderedPair(A,B).hashCode() == UnorderedPair(B,A).hashCode()"
+	 *  
+	 *   @return l'égalité entre les deux paires. 
+	 *   
+	 *   Si ils sont égaux, alors ils retourneront le même chiffre.
+	 *   Si ils ne sont pas égaux, alors les objets sont différents. **/
+	
 	public int hashCode() {
 		return this.first.hashCode() + this.second.hashCode();
 	}
