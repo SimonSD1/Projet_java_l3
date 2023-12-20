273136e17257a413b8a4da4cd67f48b7e930bf51
Ada-jm <148342280+Ada-jm@users.noreply.github.com>
2023-12-20 13:18:48 +0100

    Update SyntaxErrorException.java
    Commentaires ajoutés

--- SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/SyntaxErrorException.java
+++ SimonSepiol_JulesHerrmann_Ada/src/up/mi/ssdjha/projet/SyntaxErrorException.java
@@ -4,24 +4,47 @@ public class SyntaxErrorException extends RuntimeException{
 	private boolean lineKnown;
 	private int lineNumber;
 	private String line;
+
+	/**Constructeur de la classe SyntaxErrorException, avec initialisation
+	 * des paramètres.**/
+	
 	public SyntaxErrorException(String message,int lineNb, String line){
 		super("Syntax error on line "+lineNb+" : "+line+"\n"+message);
 		this.lineKnown = true;
 		this.lineNumber = lineNb;
 		this.line = line;
 	}
+
+	/** Ici, on considère qu'il y a une erreur à l'avance, et
+	 * le message affiché non le confimera. D'où le "false" de lineKnown,
+	 * le "-1" de lineNumber, et le "null" de line.**/
+	
 	public SyntaxErrorException(String message){
 		super(message);
 		this.lineKnown = false;
 		this.lineNumber = -1;
 		this.line = null;
 	}
+
+	/**
+	 * @return True si la ligne est connu.
+	 **/
+	
 	public boolean lineIsKnown(){
 		return this.lineKnown;
 	}
+
+	/**
+	 * @return la ligne en chaîne de caractères.
+	 **/
+	
 	public String getLine(){
 		return this.line;
 	}
+
+	/**
+	 * @return le numéro de ligne.
+	 **/
 	public int getLineNumber(){
 		return this.lineNumber;
 	}
